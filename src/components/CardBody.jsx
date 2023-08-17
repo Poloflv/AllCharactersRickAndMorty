76c39ac5fe2fba706f94cfc19cb290bcea97e0e7
Poloflv <kevinpolo316@gmail.com>
2023-08-17 10:34:19 -0500

    change status color

--- src/components/CardBody.jsx
+++ src/components/CardBody.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const CardBody = ({character}) => {
+  const statusP = {
+    Alive: "bg-green-500",
+        Dead: "bg-red-500",
+        unknown: "bg-amber-900"
+  }
+  
   return (
+
+    
     <section className='bg-[url(/fondo7.jpg)] bg-no-repeat bg-cover bg-bottom bg-transparent/90 h-[420px] rounded-2xl overflow-hidden'>
         <div >
             <img  src={character.image} alt="" />
@@ -10,7 +18,7 @@ const CardBody = ({character}) => {
                     <li>Name: {character.name}</li>
                     <li>Gender: {character.gender}</li>
                     <li>Location: {character.location.name}</li>
-                    <li className='inline-flex'>Location: {character.status} <span className='ml-2'><img className='animate-pulse w-6' src="/red.png" alt="" /></span></li>
+                    <li className='inline-flex'>Location: {character.status} <span className='ml-2 mt-[5px]'> <div className={`h-[15px] aspect-square animate-pulse ${statusP[character?.status]} rounded-full`}></div></span></li>
 
                 </ul>
             </div>
